7617c280bd6e197a60111e8e7ae10591801bd27f
yoyo1327 <yoyo_1327@hotmail.com>
2025-11-01 15:41:39 +0100

    Update 02_data_quality

--- 02_data_quality.sql
+++ 02_data_quality.sql
@@ -1,4 +1,4 @@
--- Check for missing values
+-- 1.1 Check for missing values
 -- Count NULLs per columns
 SELECT COUNT(*) AS total_rows,
 	SUM(CASE WHEN date IS NULL THEN 1 ELSE 0 END) AS missing_date,
@@ -26,7 +26,7 @@ SELECT COUNT(*) AS total_rows,
 	SUM(CASE WHEN seasonality_index IS NULL THEN 1 ELSE 0 END) AS missing_seasonality_index
 FROM external_factors;
 
--- Identify any date gaps in the data
+-- 1.2 Identify any date gaps in the data
 -- Using LAG() window function to get the previous date; if date gap > 1 then there is a date gap
 -- Missing 2023-10-29 and 2024-10-27 in three tabels
 WITH date_diff as (
@@ -71,4 +71,47 @@ ORDER BY date
 	FROM date_diff
 	WHERE date_diff > 1;
 
--- Find outliner in spend and revenue
+-- 1.3 Find outliner in spend and revenue
+-- Detect outliers in spend and revenue using IQR
+-- Values outside (Q1 - 1.5*IQR, Q3 + 1.5*IQR) as outliers
+WITH combined AS (
+  SELECT date, col_name, value
+  FROM marketing_spend
+  CROSS JOIN LATERAL (
+	VALUES 
+	  ('paid_search_spend', paid_search_spend),
+	  ('paid_social_spend', paid_social_spend),
+	  ('display_spend', display_spend),
+	  ('email_spend', email_spend),
+	  ('affiliate_spend', affiliate_spend),
+	  ('tv_spend', tv_spend)
+  ) AS t(col_name, value)
+  UNION ALL
+  SELECT date, col_name, value
+  FROM revenue
+  CROSS JOIN LATERAL (
+	VALUES
+	  ('revenue', revenue),
+	  ('transactions', transactions),
+	  ('new_customers', new_customers)
+  ) AS t(col_name, value)
+
+-- Compute IQR for each column
+), iqr_stats AS (
+  SELECT col_name,
+	percentile_cont(0.25) WITHIN GROUP (ORDER BY value) AS q1,
+	percentile_cont(0.75) WITHIN GROUP (ORDER BY value) AS q3
+  FROM combined
+  GROUP BY col_name
+	
+-- Detect outliers
+)
+  SELECT c.date,
+	c.col_name,
+	c.value
+  FROM combined c
+  LEFT JOIN iqr_stats s
+	ON c.col_name = s.col_name
+  WHERE c.value < (s.q1 - 1.5 * (s.q3 - s.q1))
+	OR c.value > (s.q3 + 1.5 * (s.q3 - s.q1))
+  ORDER BY c.date, c.col_name;
